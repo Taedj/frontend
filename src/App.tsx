9ee06a6b8e5c71453cce6c313e25dfaf408b12c5
bensouici akram <bensouiciakram@gmail.com>
2025-01-05 13:22:39 +0100

    implement layout with styled-components and add Home component background

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import styled from 'styled-components'
 import './App.css'
+import { dimensions } from './constants/constants'
 import SideBar from './components/SideBar/SideBar'
+import Home from './components/Home/Home'
+
+const MainContainer = styled.div`
+  display:grid;
+  grid-template-columns: ${dimensions.sideBarWidth} 1fr;
+`
 
 function App() {
   return (
     <>
-      <SideBar/>
+      <MainContainer>
+        <SideBar/>
+        <div>
+          <Home/>
+        </div>
+      </MainContainer>
     </>
   )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import engineeringBackground from '../../assets/pexels-olly-3817858.jpg'
+
+
+const HomeContainer = styled.div`
+  position:relative;
+  width:100%;
+  height:100vh
+`
+
+const HomeInner = styled.div`
+  background-image: url(${engineeringBackground});
+  height:100vh;
+  background-size: cover;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7); 
+    z-index: 1; 
+  }
+`
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <HomeContainer>
+      <HomeInner></HomeInner>
+    </HomeContainer>
   )
 }
 
