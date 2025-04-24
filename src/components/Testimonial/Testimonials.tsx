b0a15cbb6e6a35afa55f4eddd21f4df985388672
Bensouici Akram <bensouiciakram@gmail.com>
2025-04-24 09:21:28 +0100

    adding 2nd phase changes into

--- src/components/Testimonial/Testimonials.tsx
+++ src/components/Testimonial/Testimonials.tsx
@@ -4,12 +4,14 @@ import BackgroundText from '../BackgroundText/BackgroundText'
 import TestimonialBox from './TestimonialBox'
 import { colors,fontSettings } from '../../constants/constants'
 import image1 from '../../assets/images/danxavier.jpg'
+import image2 from '../../assets/images/negro.jpg'
 import RadioButtonsList from './RadioButtonsList'
-
+import Carousel from './Carousel'
 
 const MainContainer = styled.div`
   background-color:${colors.backgroundDarkColor};
   font-family:${fontSettings.fontFamily};
+  padding:7.2rem 4.8rem;
 `
 
 const ButtonsContainer = styled.div`
@@ -17,14 +19,43 @@ const ButtonsContainer = styled.div`
   justify-content:center;
 `
 
+const TestimonialsContainer = styled(Carousel)`
+  margin-top:4.8rem;
+`
+
 
 const Testimonials = () => {
   return (
     <MainContainer id="Testimonials">
       <BackgroundText backgroundText='TESTIMONIAL' innerText='Client Speak'/>
-      <TestimonialBox image={image1} title="Daniel Xavier" subTitle="User from Spain" testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."/>
+      <TestimonialsContainer deviceType='desktop'>
+        <TestimonialBox 
+          image={image1} 
+          title="Daniel Xavier" 
+          subTitle="User from Spain" 
+          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+        />
+        <TestimonialBox 
+          image={image2} 
+          title="Daniel Xavier" 
+          subTitle="User from Spain" 
+          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+        />
+        <TestimonialBox 
+          image={image1} 
+          title="Daniel Xavier" 
+          subTitle="User from Spain" 
+          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+        />
+        <TestimonialBox 
+          image={image1} 
+          title="Daniel Xavier" 
+          subTitle="User from Spain" 
+          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+        />
+      </TestimonialsContainer>
       
-      <RadioButtonsList/>
+      {/* <RadioButtonsList/> */}
     </MainContainer>
   )
 }
