8ac45f4a2eb7ab169f782b0c87f213c42d07ab8d
Bensouici Akram <bensouiciakram@gmail.com>
2025-04-28 15:44:42 +0100

    switch into tailwind in Paragraph

--- src/components/About/Paragraph.tsx
+++ src/components/About/Paragraph.tsx
@@ -1,38 +1,20 @@
 import React from 'react'
-import styled from 'styled-components'
 import { colors } from '../../constants/constants'
-
-
-const ParagrapheMainContainer = styled.div`
-    margin-top:4.8rem;
-    font-family;
-    padding:0 1.2rem;
-`
-
-const Header = styled.h1`
-    font-size:2.8rem;
-    margin-bottom:1.6rem;
-    color:white;
-`
-
-const ColorChanger = styled.span`
-    color:${colors.primaryColor};
-    text-alight:left;
-`
-
-const LongText = styled.p`
-    color:${colors.backgroundLessTextDarkColor};
-    margin-bottom;1.6rem;
-    line-height:2.8rem;
-`
 
 const Paragraph = () => {
   return (
-    <ParagrapheMainContainer>
-        <Header>I'm <ColorChanger>Tidjani Zitouni,</ColorChanger> a Mechanical Engineer</Header>
-        <LongText>I help you build brand for your business at an affordable price. Thousands of clients have procured exceptional results while working with our dedicated team. when an unknown printer took a galley of type and scrambled it to make a type specimen book.</LongText>
-        <LongText>Delivering work within time and budget which meets client’s requirements is our moto. Lorem Ipsum has been the industry's standard dummy text ever when an unknown printer took a galley.</LongText>
-    </ParagrapheMainContainer>
+    <div className='mt-[4.8rem] py-0 px-[1.2rem]'>
+        <h1 className='text-[2.8rem] mb-[1.6rem] text-white'>
+            I'm <span className='text-left' style={{color:colors.primaryColor}}>Tidjani Zitouni,</span> a Mechanical Engineer
+        </h1>
+        <p className='mb-[1.6rem] leading-[2.8rem]' style={{color:colors.backgroundLessTextDarkColor}}>
+            I help you build brand for your business at an affordable price. Thousands of clients have procured exceptional results while working with our dedicated team. when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <p className='mb-[1.6rem] leading-[2.8rem]' style={{color:colors.backgroundLessTextDarkColor}}>
+            Delivering work within time and budget which meets client’s requirements is our moto. Lorem Ipsum has been the industry's standard dummy text ever when an unknown printer took a galley.
+        </p>
+    </div>
+
   )
 }
 
