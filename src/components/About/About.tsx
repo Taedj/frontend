b4875ed199394e928bd18b8fc400a6c3428be016
Bensouici Akram <bensouiciakram@gmail.com>
2025-04-28 16:23:20 +0100

    deleteing unnecessary code

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styled from 'styled-components'
 import BackgroundText from '../BackgroundText/BackgroundText'
 import Paragraph from './Paragraph'
 import VerticalList from './VerticalList'
@@ -7,44 +6,9 @@ import HorizontalList from './HorizontalList'
 import { colors ,fontSettings} from '../../constants/constants'
 
 
-
-const AboutMainContainer = styled.div`
-  background-color:${colors.backgroundDarkColor};
-  font-family:${fontSettings.fontFamily};
-  padding:7.2rem 4.8rem;
-  font-size:1.6rem;
-  color:white;
-`
-
-const BodyContainer = styled.div`
-  display:flex;
-  width:100%;
-`
-
-const ParagraphContainer = styled.div`
-  flex:3;
-  min-width:0;
-`
-const VerticalListContainer = styled.div`
-  flex:2;
-  min-width:0;
-`
-
 const About = () => {
   return (
     <>
-      {/* <AboutMainContainer id="About-Me">
-        <BackgroundText backgroundText='ABOUT ME' innerText='Know Me More'/>
-        <BodyContainer>
-          <ParagraphContainer>
-            <Paragraph/>
-          </ParagraphContainer>
-          <VerticalListContainer>
-            <VerticalList/>
-          </VerticalListContainer>
-        </BodyContainer>
-        <HorizontalList/>
-      </AboutMainContainer> */}
       <div id='About-me' className='py-[7.2rem] px-[4.8rem] text-[1.6rem] text-white' style={{backgroundColor:colors.backgroundDarkColor,fontFamily:fontSettings.fontFamily}}>
         <BackgroundText backgroundText='ABOUT ME' innerText='Know Me More'/>
         <div className='flex w-full'>
