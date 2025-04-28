b062c069368594bc4575edc4243761f384b569fb
Bensouici Akram <bensouiciakram@gmail.com>
2025-04-28 15:37:56 +0100

    adding unnecessary code

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -6,83 +6,8 @@ import engineeringBackground from '../../assets/pexels-olly-3817858.jpg'
 import Button from '../Button/Button'
 import Chevron from '../Chevron/Chevron'
 
-
-const HomeContainer = styled.div`
-  position:relative;
-  width:100%;
-  height:100vh;
-  font-family:${fontSettings.fontFamily};
-  color:white;
-`
-
-const HomeInner = styled.div`
-  display:flex;
-  flex-direction:column;
-  justify-content:center;
-  background-attachment: fixed;
-  height:100vh;
-  background-size: cover;
-  align-items:center;
-  background-image: url(${engineeringBackground});
-
-  &::before {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.7); 
-    z-index: 1; 
-  }
-`
-
-const TopHeader = styled.p`
-  font-size:2.8rem;
-  line-height:5rem;
-  font-weight:500;
-  z-index:2;
-  margin: 0 0 1.6rem 0;
-`
-
-const Address = styled.p`
-  font-size:2.1rem;
-  z-index:2;
-  color:${colors.whiteWithOpacity}
-`
-
-const TypeWriterContainer = styled.div`
-  font-size: 6rem;
-  z-index:2;
-  height:6rem;
-  font-weight:600
-`
-
-
 const Home = () => {
   return (
-    // <HomeContainer id="Home">
-    //   <HomeInner>
-    //     <TopHeader>Welcome</TopHeader>
-    //     <TypeWriterContainer>
-    //       <Typewriter 
-    //         words={specialities}
-    //         typeSpeed={70}
-    //         loop={0}
-    //       />
-    //     </TypeWriterContainer>
-    //     <Address>based on Khroub,Constantine</Address>
-    //     <Button 
-    //       color={colors.primaryColor}
-    //       width="14.5rem"
-    //       height="5.1rem"
-    //       filledBackground={false}
-    //       hoverBackground={colors.primaryColor}
-    //       borderWidth="2px"
-    //     >Hire Me</Button>
-    //   </HomeInner>
-    //   <Chevron/>
-    // </HomeContainer>
     <div id="Home" className='relative w-full h-screen text-white'
       style={{
         fontFamily:fontSettings.fontFamily
@@ -123,5 +48,3 @@ const Home = () => {
 }
 
 export default Home
-
-// TODO 1: delete the reps in z-index for topheader,address and typewriter
