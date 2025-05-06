479446a2e5f6da6b3c8e0a5f1be6bba04ba98167
Bensouici Akram <bensouiciakram@gmail.com>
2025-05-06 16:09:59 +0100

    migrate footer component into tailwind

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,43 +1,22 @@
-import React from 'react'
-import styled from 'styled-components'
 import { colors,fontSettings } from '../../constants/constants'
 
-const MainContainer = styled.div`
-  font-family:${fontSettings.fontFamily};
-  display:flex;
-  justify-content:space-between;
-  background-color:${colors.backgroundDarkColor};
-  color:${colors.backgroundLessTextDarkColor};
-  padding:6.6rem 4.8rem;
-  font-size:1.6rem;
-  font-weight:550;
-`
-
-const ColorChanger = styled.span`
-  color:${colors.primaryColor};
-`
-
-const CopyWrightContainer = styled.div`
-
-`
-
-const PolicyAndDisclaimerContainer = styled.div`
-  display:flex;
-  justify-content:flex-end;
-  & span {
-    padding:0 1.6rem;
-  }
-`
 
 const Footer = () => {
   return (
-    <MainContainer>
-      <CopyWrightContainer>Copyright © 2025 <ColorChanger>Zitouni</ColorChanger>. All Rights Reserved.</CopyWrightContainer>
-      <PolicyAndDisclaimerContainer>
-        <span>Terms & Policy</span>
-        <span>Disclaimer</span>
-      </PolicyAndDisclaimerContainer>
-    </MainContainer>
+    <div 
+      className='flex justify-between py-[6.6rem] px-[4.8rem] text-[1.6rem]'
+      style={{
+        fontFamily:fontSettings.fontFamily,
+        backgroundColor:colors.backgroundDarkColor,
+        color:colors.backgroundLessTextDarkColor
+      }}    
+    >
+      <div>Copyright © 2025 <span style={{color:colors.primaryColor}}>Zitouni</span>. All Rights Reserved.</div>
+      <div className='flex justify-end'>
+        <span className='py-0 px-[1.6rem]'>Terms & Policy</span>
+        <span className='py-0 px-[1.6rem]'>Disclaimer</span>
+      </div>
+    </div>
   )
 }
 
