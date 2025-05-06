deae8c6b112ee24166105a3d62cf68bb1b471f0a
Bensouici Akram <bensouiciakram@gmail.com>
2025-05-06 15:30:24 +0100

    migrate Testimonials into tailwind

--- src/components/Testimonial/Testimonials.tsx
+++ src/components/Testimonial/Testimonials.tsx
@@ -5,53 +5,40 @@ import TestimonialBox from './TestimonialBox'
 import { colors,fontSettings } from '../../constants/constants'
 import image1 from '../../assets/images/danxavier.jpg'
 import image2 from '../../assets/images/negro.jpg'
-import RadioButtonsList from './RadioButtonsList'
 import Carousel from './Carousel'
-
-const MainContainer = styled.div`
-  background-color:${colors.backgroundDarkColor};
-  font-family:${fontSettings.fontFamily};
-  padding:7.2rem 4.8rem;
-`
-
-const ButtonsContainer = styled.div`
-  display:flex;
-  justify-content:center;
-`
-
-const TestimonialsContainer = styled(Carousel)`
-  margin-top:4.8rem;
-  padding: 0 20px; /* Add horizontal padding */
-`
-
 
 const Testimonials = () => {
   return (
-    <MainContainer id="Testimonials">
+    <div id='Testimonials' className='py-[7.2rem] px-[4.8rem]'
+      style={{
+        backgroundColor:colors.backgroundDarkColor,
+        fontFamily:fontSettings.fontFamily
+      }}
+    >
       <BackgroundText backgroundText='TESTIMONIAL' innerText='Client Speak'/>
-      <TestimonialsContainer deviceType='desktop'>
-        <TestimonialBox 
-          image={image1} 
-          title="Daniel Xavier" 
-          subTitle="User from Spain" 
-          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
-        />
-        <TestimonialBox 
-          image={image2} 
-          title="Daniel Xavier" 
-          subTitle="User from Spain" 
-          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
-        />
-        <TestimonialBox 
-          image={image1} 
-          title="Daniel Xavier" 
-          subTitle="User from Spain" 
-          testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
-        />
-      </TestimonialsContainer>
-      
-      {/* <RadioButtonsList/> */}
-    </MainContainer>
+      <div className='mt-[4.8rem] py-0 px-[20px]'>
+        <Carousel deviceType='desktop'>
+          <TestimonialBox 
+            image={image1} 
+            title="Daniel Xavier" 
+            subTitle="User from Spain" 
+            testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+          />
+          <TestimonialBox 
+            image={image2} 
+            title="Daniel Xavier" 
+            subTitle="User from Spain" 
+            testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+          />
+          <TestimonialBox 
+            image={image1} 
+            title="Daniel Xavier" 
+            subTitle="User from Spain" 
+            testomonial="Easy to use, reasonably priced simply dummy text of the printing and typesetting industry. Quidam lisque persius interesset his et, in quot quidam possim iriure."
+          />
+        </Carousel>
+      </div>
+    </div>
   )
 }
 
