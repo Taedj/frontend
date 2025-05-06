35aebf94ec61108e7dbbd4e515dd44be075d3041
Bensouici Akram <bensouiciakram@gmail.com>
2025-05-06 15:46:42 +0100

    migrate AddressBox into tailwind

--- src/components/Contact/AddressBox.tsx
+++ src/components/Contact/AddressBox.tsx
@@ -1,101 +1,60 @@
-import React from 'react'
-import styled from 'styled-components'
 import { FaPhoneAlt,FaFax  } from "react-icons/fa";
 import { IoMdMail  } from "react-icons/io";
 import { colors } from '../../constants/constants';
 import {FbIcon,TwIcon,GwIcon,GmIcon} from '../SideBar/SideBar'
 
-
-const AddressContainer = styled.div`
-`
-
-const AddressLinesContainer = styled.div`
-    margin-bottom:2.4rem;
-`
-
-const AddressTitle = styled.h1`
-    font-size:2.1rem;
-    font-weight:500;
-    margin:0;
-    margin-bottom:1.6rem;
-`
-
-const AddressLine = styled.p`
-    color:${colors.backgroundLessTextDarkColor};
-    font-size:1.6rem;
-    font-weight:400;
-    line-height:2.88rem;
-`
-
-const IconContainer = styled.span`
-    margin-right:0.8rem;
-`
-
-const InfosContainer = styled.div`
-    margin-bottom:2.4rem;
-`
-
-const InfoContainer = styled.div`
-    color:${colors.backgroundLessTextDarkColor};
-    display:flex;
-    font-size:1.6rem;
-    font-weight:400;
-    line-height:2.88rem;
-`
-
-const SocialMediasList = styled.ul`
-    width:14rem;
-    display:flex;
-    justify-content:space-between;
-    list-style:none;
-    padding:0;
-`
-
-const SocialMediaItem = styled.li`
-
-`
-
-
-
 const AddressBox = () => {
   return (
-    <AddressContainer>
-        <AddressTitle>ADDRESS</AddressTitle>
-        <AddressLinesContainer>
-            <AddressLine>4th Floor, Plot No.22,</AddressLine>
-            <AddressLine>145 Murphy Canyon Rd.</AddressLine>
-            <AddressLine>San Diego CA 2028</AddressLine>
-        </AddressLinesContainer>
-        <InfosContainer>
-            <InfoContainer>
-                <IconContainer><FaPhoneAlt size={14} color={colors.primaryColor}/></IconContainer>
-                <InfoContainer>(060) 444 434 444</InfoContainer>
-            </InfoContainer>
-            <InfoContainer>
-                <IconContainer><FaFax size={14} color={colors.primaryColor}/></IconContainer>
-                <InfoContainer>(060) 555 545 555</InfoContainer>
-            </InfoContainer>
-            <InfoContainer>
-                <IconContainer><IoMdMail size={14} color={colors.primaryColor}/></IconContainer>
-                <InfoContainer>chat@simone.com</InfoContainer>
-            </InfoContainer>
-        </InfosContainer>
-        <AddressTitle>FOLLOW ME</AddressTitle>
-        <SocialMediasList>
-          <SocialMediaItem>
-            <FbIcon size={14} />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <TwIcon size={14} />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <GwIcon size={14} />
-          </SocialMediaItem>
-          <SocialMediaItem>
-            <GmIcon size={14} />
-          </SocialMediaItem>
-        </SocialMediasList>
-    </AddressContainer>
+    <div>
+      <h1 className='text-[2.1rem] font-medium m-0 mb-[1.6rem]'>ADDRESS</h1>
+      <div className='mb-[2.4rem]'>
+        <p className='text-[1.6rem] font-normal leading-[2.88rem]' 
+          style={{color:colors.backgroundLessTextDarkColor}}>
+          4th Floor, Plot No.22,
+        </p>
+        <p className='text-[1.6rem] font-normal leading-[2.88rem]' 
+          style={{color:colors.backgroundLessTextDarkColor}}>
+            145 Murphy Canyon Rd.
+        </p>
+        <p className='text-[1.6rem] font-normal leading-[2.88rem]' 
+          style={{color:colors.backgroundLessTextDarkColor}}>
+            San Diego CA 2028
+        </p>
+      </div>
+      <div className='mb-[2.4rem]'>
+        <div className='flex text-[1.6rem] font-medium leading-[2.88rem]'
+          style={{
+            color:colors.backgroundLessTextDarkColor
+          }}
+        >
+          <span className='mr-[2.4rem]'><FaPhoneAlt size={14} color={colors.primaryColor}/></span>
+          <div className='flex justify-between list-none p-0' style={{color:colors.backgroundLessTextDarkColor}}>(060) 444 434 444</div>
+        </div>
+        <div className='flex text-[1.6rem] font-medium leading-[2.88rem]'
+          style={{
+            color:colors.backgroundLessTextDarkColor
+          }}
+        >
+          <span className='mr-[2.4rem]'><FaFax size={14} color={colors.primaryColor}/></span>
+          <div className='flex justify-between list-none p-0' style={{color:colors.backgroundLessTextDarkColor}}>(060) 555 545 555</div>
+        </div>
+        <div className='flex text-[1.6rem] font-medium leading-[2.88rem]'
+          style={{
+            color:colors.backgroundLessTextDarkColor
+          }}
+        >
+          <span className='mr-[2.4rem]'><IoMdMail size={14} color={colors.primaryColor}/></span>
+          <div className='flex justify-between list-none p-0' style={{color:colors.backgroundLessTextDarkColor}}>chat@simone.com</div>
+        </div>
+      </div>
+      <h1 className='text-[2.1rem] font-semibold m-0 mb-[1.6rem]'>FOLLOW ME</h1>
+      <ul className='flex justify-between list-none w-[14rem] p-0'>
+          <li><FbIcon size={14} /></li>
+          <li><TwIcon size={14} /></li>
+          <li><GwIcon size={14} /></li>
+          <li><GmIcon size={14} /></li>
+      </ul>
+    </div>
   )
 }
 
