617c689dcf8467c713b202469b9d9a49209f08c9
Bensouici Akram <bensouiciakram@gmail.com>
2025-05-06 14:47:01 +0100

    migrate categoriesSelector component into tailwind

--- src/components/Portfolio/CategoriesSelector.tsx
+++ src/components/Portfolio/CategoriesSelector.tsx
@@ -1,32 +1,18 @@
-import React from 'react'
-import styled from 'styled-components'
 import { categories,colors,fontSettings } from '../../constants/constants'
 
-const MainContainer = styled.div`
-  font-family:${fontSettings.fontFamily};
-  font-size:1.6rem;
-  font-weight:400;
-  color:${colors.categoriesSelectorColor};
-  margin:4.8rem 0;
-`
-
-const Categories = styled.ul`
-  display:flex;
-  list-style:none;
-  justify-content:center;
-`
-
-const Category = styled.li`
-  padding:0.96rem 1.6rem;
-`
 
 const CategoriesSelector = () => {
   return (
-    <MainContainer>
-      <Categories>
-        {categories.map((categoryText) => <Category>{categoryText}</Category>)}
-      </Categories>
-    </MainContainer>
+    <div className='text-[1.6rem] font-normal my-[4.8rem] mx-0'
+      style={{
+        fontFamily:fontSettings.fontFamily,
+        color:colors.categoriesSelectorColor
+      }}
+    >
+      <ul className='flex list-none justify-center'>
+        {categories.map((categoryText) => <li className='py-[0.96rem] px-[1.6rem]'>{categoryText}</li>)}
+      </ul>
+    </div>
   )
 }
 
