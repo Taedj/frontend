2a98b44bcbdbb14fd360e8670574a5ce1c9a403c
Bensouici Akram <bensouiciakram@gmail.com>
2025-05-07 16:24:36 +0100

    fixing font weight of the background text

--- src/components/BackgroundText/BackgroundText.tsx
+++ src/components/BackgroundText/BackgroundText.tsx
@@ -9,8 +9,8 @@ interface Props {
 const BackgroundText = ({backgroundText,innerText}:Props) => {
   return (
     <div className='relative flex flex-col p-0 m-0 justify-center align-center text-center text-white'>
-        <h2 className='m-0 text-[13.2rem] opacity-10' style={{color:colors.backgroundTextDarkColor}}>{backgroundText}</h2>
-        <p className='absolute flex flex-col self-center text-[3.6rem] '>
+        <h2 className='m-0 text-[13.2rem] opacity-10' style={{color:colors.backgroundTextDarkColor,fontWeight:550}}>{backgroundText}</h2>
+        <p className='absolute flex flex-col self-center text-[3.6rem]'>
           {innerText}
           <span className='block mx-[31.5rem] w-[8rem] h-[3px] leading-[5.4rem] ' style={{backgroundColor:colors.primaryColor}}></span>
         </p>
