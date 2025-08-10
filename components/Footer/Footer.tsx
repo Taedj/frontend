073f5f0d8aa0c13a5e1dfb9786600aeb664c52f8
bensouici akram <bensouiciakram@gmail.com>
2025-08-10 11:23:53 +0100

    Refactor Footer component to eliminate isMobile prop and improve responsiveness with Tailwind CSS

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,31 +1,17 @@
 import { colors,fontSettings } from '../../constants/constants'
 
-const getMainContainerClass = (isMobile:boolean) => {
-  if (isMobile) return 'flex flex-col justify-between py-[6.6rem] px-[4.8rem] text-[1.6rem] ';
-  return 'flex justify-between py-[6.6rem] px-[4.8rem] text-[1.6rem]';
-}
-
-const getLinkContainerClass = (isMobile:boolean) => {
-  if (isMobile) return 'flex justify-center'; 
-  return 'flex justify-end';
-}
-
-interface Props {
-  isMobile:boolean;
-}
-
-const Footer = ({isMobile}:Props) => {
+const Footer = () => {
   return (
     <div 
-      className={getMainContainerClass(isMobile)}
+      className='flex max-md:flex-col justify-between py-[6.6rem] px-[4.8rem] text-[1.6rem] '
       style={{
         fontFamily:fontSettings.fontFamily,
         backgroundColor:colors.backgroundDarkColor,
         color:colors.backgroundLessTextDarkColor
       }}    
     >
-      <div className={(isMobile)?'!mb-[1.6rem] text-center':''}>Copyright © 2025 <span style={{color:colors.primaryColor}}>Zitouni</span>. All Rights Reserved.</div>
-      <div className={getLinkContainerClass(isMobile)}>
+      <div className='max-md:!mb-[1.6rem] max-md:text-center'>Copyright © 2025 <span style={{color:colors.primaryColor}}>Zitouni</span>. All Rights Reserved.</div>
+      <div className='flex max-md:justify-center md:justify-end'>
         <span className='py-0 px-[1.6rem]'>Terms & Policy</span>
         <span className='py-0 px-[1.6rem]'>Disclaimer</span>
       </div>
