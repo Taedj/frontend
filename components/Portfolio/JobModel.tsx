2918d23921b0de86fd4e971a68c36a2e8b2bd68c
bensouici akram <bensouiciakram@gmail.com>
2025-08-25 10:27:45 +0100

    restore the JobModel css and tsx file

--- components/Portfolio/JobModel.tsx
+++ components/Portfolio/JobModel.tsx
@@ -30,26 +30,37 @@ const JobModel = ({title,images,description,onClose}:Props) => {
         arrows: false
     };
     return (
-        <div className="w-full md:w-1/2 mt-6 md:mt-0 px-4 sm:px-6">
-        <h2 className="text-lg sm:text-xl md:text-3xl font-semibold mb-2 sm:mb-4">Project Info:</h2>
-        <p className="text-sm sm:text-base md:text-xl leading-relaxed sm:leading-8">
-            {description}
-        </p>
-
-        <h2 className="text-lg sm:text-xl md:text-3xl font-semibold mt-6 sm:mt-10 mb-2 sm:mb-3">
-            Project Details:
-        </h2>
-        <ul className="mb-6 text-sm sm:text-base md:text-xl">
-            <li className="py-2 sm:py-3 border-b border-b-border">
-            <span className="font-semibold">Client:</span> Ruby Clinton
-            </li>
-            <li className="py-2 sm:py-3 border-b border-b-border">
-            <span className="font-semibold">Industry:</span> Art & Design
-            </li>
-            <li className="py-2 sm:py-3 border-b border-b-border">
-            <span className="font-semibold">Date:</span> July 16,2019
-            </li>
-        </ul>
+            <div className='h-[80vh] p-6 rounded-[8px] bg-job-model text-cell-description'>
+                <div className='flex justify-end' onClick={onClose}><RxCross1 size={20}/></div>
+                <h1 className='text-center text-4xl mb-10 font-semibold'>{title}</h1>
+                <div className='flex max-md:flex-col px-5'>
+                    <div className={`slider-container ${breakpoint ? 'mobile' : 'desktop'}`} style={{ width: sliderWidth }}>
+                        <Slider {...settings} className="h-full">     
+                            {
+                                images.map(image => (
+                                    <div key={image} className="slide-item">
+                                        <div  className='h-full w-full'>
+                                            <img 
+                                                src={image} 
+                                                alt={title}
+                                            />
+                                        </div>
+                                    </div>
+                                ))
+                            }                   
+                        </Slider>
+                    </div>
+                    <div className='max-md:mt-20 px-10'>
+                        <h2 className='text-3xl font-semibold'>Project Info:</h2>
+                        <p className='text-2xl leading-12'>{description}</p>
+                        <h2 className='text-3xl font-semibold mt-10 mb-3'>Project Details:</h2>
+                        <ul className='mb-6'>
+                            <li className='py-5 text-2xl border-b border-b-border'><span className='font-semibold'>Client:</span> Ruby Clinton</li>
+                            <li className='py-5 text-2xl border-b border-b-border' ><span className='font-semibold'>Industry:</span> Art & Design</li>
+                            <li className='py-5 text-2xl border-b border-b-border'><span className='font-semibold'>Date:</span> July 16,2019</li>
+                        </ul>
+                    </div>
+                </div>
         </div>
     )
     }
