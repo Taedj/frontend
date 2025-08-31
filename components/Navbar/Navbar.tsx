a20b1017138bf72eba2e5189eef2e3406aec0091
bensouici akram <bensouiciakram@gmail.com>
2025-08-31 13:15:12 +0100

    adding signin and signup into mobile and tablet view

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -4,11 +4,16 @@ import { RxCross1 } from "react-icons/rx";
 import { colors, sections } from "../../constants/constants";
 import useConfig from "../../hooks/useConfig";
 import SocialMedias from "../SideBar/SocialMedias";
+import useIsLogged from "../../hooks/useIsLogged";
+import useLogout from "../../hooks/useLogout";
+import Link from "next/link";
 
 const Navbar = () => {
   const { data: config } = useConfig();
   const [menuClicked, setMenuClicked] = useState(false);
   const [HoveredIndex, setHoveredIndex] = useState(0);
+  const { isLogged } = useIsLogged();
+  const { mutate } = useLogout();
   return (
     <>
       <div className="fixed w-full top-0 left-0 z-50">
@@ -38,14 +43,13 @@ const Navbar = () => {
               {sections.map((section, index) => (
                 <li
                   key={index}
-                  className="p-3 border-b"
+                  className="p-3 border-b border-menu-item-border"
                   style={{
                     transition: "color 0.3s",
                     color:
                       HoveredIndex === index || index === 0
                         ? colors.primaryColor
                         : "white",
-                    borderColor: colors.menuItemBorderColor,
                   }}
                   onMouseEnter={() => setHoveredIndex(index)}
                   onMouseLeave={() => setHoveredIndex(0)}
@@ -55,6 +59,29 @@ const Navbar = () => {
                   </a>
                 </li>
               ))}
+              {!isLogged && (
+                <>
+                  <li
+                    key={sections.length}
+                    className="hover:text-primary p-3 border-b border-menu-item-border"
+                  >
+                    <Link href="/signin">Sign-in</Link>
+                  </li>
+                  <li
+                    key={sections.length + 1}
+                    className="hover:text-primary p-3"
+                  >
+                    <Link href="/signup">Sign-up</Link>
+                  </li>
+                </>
+              )}
+              {isLogged && (
+                <li key={sections.length} className="hover:text-primary p-3">
+                  <a href="/" onClick={() => mutate()}>
+                    Logout
+                  </a>
+                </li>
+              )}
             </ul>
           </div>
         )}
