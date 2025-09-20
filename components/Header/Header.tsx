4e49c67a6a225a1dde1046d6d5f929c0abe9c7fb
DESKTOP-05V5OG3\zitou <zitounitidjani@gmail.com>
2025-09-20 01:26:22 +0100

    remove header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,13 +1,13 @@
 "use client";
 import Link from "next/link";
-import { colors } from "../../constants/constants";
+
 import useIsLogged from "../../hooks/useIsLogged";
 
 const Header = () => {
   const { isLogged } = useIsLogged();
 
   return (
-    <div className="hidden lg:flex justify-end items-center p-4 bg-transparent absolute top-0 right-0 w-full z-50">
+    <div className="hidden lg:flex justify-end items-center px-8 py-4 bg-transparent absolute top-0 right-0 w-full z-50">
       {!isLogged && (
         <div className="text-xl font-semibold">
           <span className="mr-4" style={{ color: "white" }}>
