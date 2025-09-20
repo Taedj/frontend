ff437037456b49f2979fab21e749c05e5e84f64b
DESKTOP-05V5OG3\zitou <zitounitidjani@gmail.com>
2025-09-20 01:24:23 +0100

    remove header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,13 +7,13 @@ const Header = () => {
   const { isLogged } = useIsLogged();
 
   return (
-    <div className="hidden lg:flex justify-end items-center p-4 bg-transparent absolute top-0 right-0 w-full">
+    <div className="hidden lg:flex justify-end items-center p-4 bg-transparent absolute top-0 right-0 w-full z-50">
       {!isLogged && (
         <div className="text-xl font-semibold">
-          <span className="mr-4" style={{ color: colors.primaryColor }}>
+          <span className="mr-4" style={{ color: "white" }}>
             <Link href="/signin">Login</Link>
           </span>
-          <span style={{ color: colors.primaryColor }}>
+          <span style={{ color: "white" }}>
             <Link href="/signup">Register</Link>
           </span>
         </div>
