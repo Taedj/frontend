f3f5fc6d5e6cf57ce3259562bea841292f8bb637
Taedj Dev <tidjanizitouni@gmail.com>
2025-10-15 18:54:17 +0100

    14

--- components/Portfolio/JobModel.tsx
+++ components/Portfolio/JobModel.tsx
@@ -37,10 +37,10 @@ const JobModel = ({ title, category, images = [], description, technologies = []
           <ImageGallery images={images} />
         </div>
         <div className="w-full md:w-2/3 mt-4 md:mt-0 px-2 sm:px-4 overflow-y-auto">
-          <h1 className="text-base font-semibold mb-2">{title}</h1>
+          <h1 className="text-[25px] font-semibold mb-2">{title}</h1>
           <p className="text-xl md:text-2xl text-gray-400 mb-4">{category}</p>
           <h2 className="text-2xl md:text-3xl font-semibold mb-2">Project Info:</h2>
-          <div className="text-sm leading-7 mb-4">
+          <div className="text-[23px] leading-7 mb-4">
             <ReactMarkdown
               components={{
                 a: ({...props}) => <a style={{fontWeight: 'bold', color: 'red', backgroundColor: 'yellow'}} {...props} />
