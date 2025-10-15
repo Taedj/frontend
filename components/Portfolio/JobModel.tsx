67375610e91ced040de205b88f4c636ddbfc3653
Taedj Dev <tidjanizitouni@gmail.com>
2025-10-15 19:09:18 +0100

    16

--- components/Portfolio/JobModel.tsx
+++ components/Portfolio/JobModel.tsx
@@ -32,14 +32,15 @@ const JobModel = ({ title, category, images = [], description, technologies = []
       <div className="flex justify-end mb-4" onClick={onClose}>
         <RxCross1 size={24} />
       </div>
-      <div className="flex flex-col md:flex-row gap-4 md:gap-8 px-2 sm:px-5 md:px-10 flex-grow">
-        <div className="w-full md:w-1/3 h-full">
-          <ImageGallery images={images} />
+      <div className="flex flex-col md:flex-row gap-4 md:gap-8 px-2 sm:px-5 md:px-10 flex-grow overflow-y-auto">
+        <div className="w-full md:w-1/3">
+            <div className="md:sticky top-0">
+                <ImageGallery images={images} />
+            </div>
         </div>
-        <div className="w-full md:w-2/3 mt-4 md:mt-0 px-2 sm:px-4 overflow-y-auto">
+        <div className="w-full md:w-2/3 mt-4 md:mt-0 px-2 sm:px-4">
           <h1 className="text-[25px] font-semibold mb-2">{title}</h1>
           <p className="text-xl md:text-2xl text-gray-400 mb-4">{category}</p>
-          <h2 className="text-2xl md:text-3xl font-semibold mb-2">Project Info:</h2>
           <div className="text-[20px] leading-loose mb-4">
             <ReactMarkdown
               components={{
